f962c85b060343685190dbf4a3f306cecc89bba3
akhand <akhandpatel3108patel@gmail.com>
2020-04-29 18:25:41 +0530

    Updates

---
+++ precache-manifest.6219958737ab06133620b5fcfa515e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5296cefe88432eceacc6901b8778f8cf",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "b61aca616633afaa5aa0",
+    "url": "/covid19/static/css/main.ca42879a.chunk.css"
+  },
+  {
+    "revision": "ea0c3fa85e7d35c1c070",
+    "url": "/covid19/static/js/2.f48e366e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.f48e366e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61aca616633afaa5aa0",
+    "url": "/covid19/static/js/main.fee3982b.chunk.js"
+  },
+  {
+    "revision": "bb08661687ef9ed76af2",
+    "url": "/covid19/static/js/runtime-main.06ba182b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
